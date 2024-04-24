1e2b73a3c43428f4568fa799ed759073beaab854
lkwavestian <lkwavestian@gmail.com>
2024-04-24 20:50:46 +0800

    feat: 测试部署

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -2,14 +2,9 @@ import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
   { text: '前端导航', link: '/nav/' },
-  { text: '千浔主页', link: 'https://fe-mm.com' },
+  { text: '千浔主页', link: 'https://fe-qianxun.com' },
   {
     text: '千浔物语',
-    link: 'https://notes.fe-mm.com',
-  },
-  { text: 'mmPlayer', link: 'https://netease-music.fe-mm.com' },
-  {
-    text: '油猴脚本',
-    link: 'https://github.com/maomao1996/tampermonkey-scripts',
+    link: 'https://notes.fe-qianxun.com',
   },
 ]
