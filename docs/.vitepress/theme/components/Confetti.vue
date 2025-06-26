9e378d1edc77afe2b74f855a65b64064596bc279
lkwavestian <lkwavestian@gmail.com>
2025-06-26 15:01:06 +0800

    fix: software/vitepress/组件/五彩纸屑组件引入异常问题

--- docs/.vitepress/theme/components/Confetti.vue
+++ docs/.vitepress/theme/components/Confetti.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
+import { onMounted } from "vue";
 import confetti from "canvas-confetti";
-import { inBrowser } from "vitepress";
 
-if (inBrowser) {
+onMounted(() =>
   /* 纸屑 */
   confetti({
     particleCount: 100,
     spread: 170,
     origin: { y: 0.6 },
-  });
-}
+  })
+);
 </script>
