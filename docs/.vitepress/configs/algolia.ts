e885aac9b9956f0efceef0f290d5404bc84f2dc9
lkwavestian <lkwavestian@gmail.com>
2025-06-30 18:29:44 +0800

    feat: software/vitepress/docsearch配置

--- docs/.vitepress/configs/algolia.ts
+++ docs/.vitepress/configs/algolia.ts
@@ -1,46 +1,46 @@
-import type { DefaultTheme } from 'vitepress'
+import type { DefaultTheme } from "vitepress";
 
 export const algolia: DefaultTheme.AlgoliaSearchOptions = {
-  appId: 'UVB82VCF2Y',
-  apiKey: '4c67197afadb283c77e084bd50618c60',
-  indexName: 'fe-mm',
-  placeholder: '搜索',
+  appId: "F1VYZATNMN",
+  apiKey: "cb53b4129260f43d5b908fff210b1ab2",
+  indexName: "doc",
+  placeholder: "搜索",
   translations: {
     button: {
-      buttonText: '搜索',
-      buttonAriaLabel: '搜索',
+      buttonText: "搜索",
+      buttonAriaLabel: "搜索",
     },
     modal: {
       searchBox: {
-        resetButtonTitle: '清除查询条件',
-        resetButtonAriaLabel: '清除查询条件',
-        cancelButtonText: '取消',
-        cancelButtonAriaLabel: '取消',
+        resetButtonTitle: "清除查询条件",
+        resetButtonAriaLabel: "清除查询条件",
+        cancelButtonText: "取消",
+        cancelButtonAriaLabel: "取消",
       },
       startScreen: {
-        recentSearchesTitle: '搜索历史',
-        noRecentSearchesText: '没有搜索历史',
-        saveRecentSearchButtonTitle: '保存至搜索历史',
-        removeRecentSearchButtonTitle: '从搜索历史中移除',
-        favoriteSearchesTitle: '收藏',
-        removeFavoriteSearchButtonTitle: '从收藏中移除',
+        recentSearchesTitle: "搜索历史",
+        noRecentSearchesText: "没有搜索历史",
+        saveRecentSearchButtonTitle: "保存至搜索历史",
+        removeRecentSearchButtonTitle: "从搜索历史中移除",
+        favoriteSearchesTitle: "收藏",
+        removeFavoriteSearchButtonTitle: "从收藏中移除",
       },
       errorScreen: {
-        titleText: '无法获取结果',
-        helpText: '你可能需要检查你的网络连接',
+        titleText: "无法获取结果",
+        helpText: "你可能需要检查你的网络连接",
       },
       footer: {
-        selectText: '选择',
-        navigateText: '切换',
-        closeText: '关闭',
-        searchByText: '搜索提供者',
+        selectText: "选择",
+        navigateText: "切换",
+        closeText: "关闭",
+        searchByText: "搜索提供者",
       },
       noResultsScreen: {
-        noResultsText: '无法找到相关结果',
-        suggestedQueryText: '你可以尝试查询',
-        reportMissingResultsText: '你认为该查询应该有结果？',
-        reportMissingResultsLinkText: '点击反馈',
+        noResultsText: "无法找到相关结果",
+        suggestedQueryText: "你可以尝试查询",
+        reportMissingResultsText: "你认为该查询应该有结果？",
+        reportMissingResultsLinkText: "点击反馈",
       },
     },
   },
-}
+};
